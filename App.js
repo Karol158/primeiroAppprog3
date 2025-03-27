3844f31432870d9c3c11085fd61ae52f34eeff99
Karol158 <159441394+Karol158@users.noreply.github.com>
2025-03-27 10:55:35 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Karoline Rayssa da Silva Gomes</Text>
+      <Text>Fernanda Lima Ribeiro da Silva</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#800080',
     alignItems: 'center',
     justifyContent: 'center',
   },
